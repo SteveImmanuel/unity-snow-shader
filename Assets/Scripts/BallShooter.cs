00a7b350f35e195ae6a5f9f12ebc8c205e667a9b
SteveImmanuel <steveandreasimanuel@gmail.com>
2021-11-10 22:21:48 +0700

    Implement shooter script

---
+++ Assets/Scripts/BallShooter.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BallShooter : MonoBehaviour
+{
+    public GameObject ballPrefab;
+    public float initialSpeed = 5f;
+
+    private void Start()
+    {
+        Destroy(gameObject, 5);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Shoot();
+        }
+    }
+
+    private void Shoot()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        GameObject ball = Instantiate(ballPrefab, transform.position, transform.rotation);
+        Rigidbody rb = ball.GetComponent<Rigidbody>();
+        rb.velocity = ray.direction * initialSpeed;
+    }
+}
